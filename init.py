24f0e77baf9f916bd960231df05b80c1e851e91e
DyerSituation <ead2174@columbia.edu>
2017-11-30 15:50:26 -0500

    init.py

--- init.py
+++ init.py
@@ -1,49 +1,58 @@
 # DataMiner
-
+from collections import defaultdict
 #code so far:
 
 
 #create list of all items
-def itemLister(dataSet):
+def c1Generator(dataSet):
   #try to use set instead?
   allItems = []
   for marketBasket in dataSet:
     for item in marketBasket:
-      if not item in allItems:
-        allItems.append(item)
-  return allItems
+      if not [item] in allItems:
+        allItems.append([item]);
+
+  allItems.sort()
+  return list(map(frozenset, allItems)) 
 
 
 #return list of items that meet support threshhold
 def supportList(Ck, Doc, threshhold):
   itemcount = defaultdict(int)
+  totalSize = len(Doc)
+  supportList = []
   for transaction in Doc:
     for candidate in Ck:
-      if candidate issubset(transaction)
+      if candidate.issubset(transaction):
         itemcount[candidate] += 1
-  for item, count in itemcount:
-    print "item:"
-    print item
-    print "count:"
-    print count
+  for item in itemcount:
+    support = float(itemcount[item])/float(totalSize)
+    if support >= threshhold:
+      supportList.append(item)
+  return supportList
+
+
 
   
 #return Ck
-def aprioriGen(lkPrev):
+#def aprioriGen(lkPrev):
 
 def main():
   
   dataSet = [[1, 3, 4], [2, 3, 5], [1, 2, 3, 5], [2, 5]]
   
-
+  Lall = []
   #proccess data 
   C1 = c1Generator(dataSet)
-  
-  print C1
-  
-
+  L1 = supportList(C1, dataSet, supportThresh)
+  Lall.append(L1)
+  k = 2
   supportThresh = .3
-  totalSize = len(myList)
+  totalSize = len(C1)
+#  while(len(Lall[k-1])>0):
+#    Ck =
+
+  print supportList(C1, dataSet, supportThresh)
   
   
   #create list of Ck
@@ -60,6 +69,6 @@ def main():
   #Check confidence
   
   #association rule maker
-  
+"""  
   
 if __name__ == '__main__': main()
