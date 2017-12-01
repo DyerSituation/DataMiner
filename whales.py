c4e172684a6094a79e4a72c920bc26ffd868b235
Sarina Xie <sx2166@columbia.edu>
2017-12-01 15:40:29 -0500

    neater?

--- whales.py
+++ whales.py
@@ -3,28 +3,15 @@ import csv
 import itertools
 from collections import defaultdict
 import copy
+from operator import itemgetter
 
 # min_sup = sys.argv[2]
 # min_conf = sys.argv[3]
 min_sup = 0.00
-min_conf = 0.5
+min_conf = 0.0
 
 
-def populate_setdict(listk):
-	#populate dictionary for k+1 itemset - itemset|frequency
-	setsk = {}
-	for row in biglist:
-		for itemset in listk:
-			itemset = set(itemset)
-			if itemset.issubset(row):
-				itemset = tuple(itemset)
-				if itemset not in setsk:
-					setsk[itemset] = 1
-				else:
-					setsk[itemset] += 1
-	return setsk
-
-def generate_list(dictk):
+def find_sets(dictk):
 	listk = []
 	for k, v in dictk.iteritems():
 		endings = itertools.combinations(v, 2)
@@ -32,13 +19,28 @@ def generate_list(dictk):
 			listk.append(list(k) + list(e))
 	return listk
 
-def generate_dissected_dict(listk):
+
+def find_freqs(listk):
+	#populate dictionary for k+1 itemset - itemset|frequency
+	setsk = {}
+	for row in biglist:
+		for itemset in listk:
+			itemset = frozenset(itemset)
+			if itemset.issubset(row):
+				itemset = frozenset(itemset)
+				if itemset not in setsk:
+					setsk[itemset] = 1
+				else:
+					setsk[itemset] += 1
+	return setsk
+
+def prune(listk):
 	dictk = defaultdict(list)
 	for item in setsk:
 		if setsk[item] >= min_sup_count:
 			freqfreqs[frozenset(item)] = setsk[item]
 			item = list(item)
-			dictk[tuple(item[:-1])].append(item[-1])
+			dictk[frozenset(item[:-1])].append(item[-1])
 			#store the frequent sets in a list for making association rules from
 			frequentsets.append(item)
 	return dictk
@@ -48,19 +50,18 @@ file_name = sys.argv[1]
 with open(file_name) as f:
 	rows = csv.reader(f)
 	biglist = list(rows)
-	#populate dictionary for k=1 itemsets|frequency
+	#populate dictionary for k=1 - itemset|frequency
 	sets1 = {}
 	for row in biglist:
 		for i in range(0, len(row)):
-			row[i] = str(i) + "-" + row[i].rstrip()		#attach column number to the item
-			#consider doing if i == column number, round(int(row[i]), 0) for some columns
+			row[i] = str(i) + "-" + row[i].rstrip()		#attach column number to the item in case same values are allowed in multiple columns
 			if row[i] not in sets1:
 				sets1[row[i]] = 1
 			else:
 				sets1[row[i]] += 1
-		row = set(row)		#convert to set to be able to use issubset() later
+		row = frozenset(row)		#convert to frozenset to be able to use issubset() later
 
-#initialize stuff for while loop, fill in listsets1
+#initialize stuff for while loop, start the k=1 case
 first = True
 frequents = 1
 frequentsets = []
@@ -79,33 +80,26 @@ while frequents > 0:
 		listk = list(itertools.combinations(list1, 2))
 		first = False
 	else:
-		listk = generate_list(dictk)	#dictk is from previous iteration
-	setsk = populate_setdict(listk)
+		listk = find_sets(dictk)
+	setsk = find_freqs(listk)
+	dictk = prune(setsk)
+	
+	frequents = len(dictk)	#if set is empty, stop
 
-	dictk = generate_dissected_dict(setsk)
-	#update loop condition
-	frequents = len(dictk)
-
-#generate all possible association rules that follow the format {a,b,c,...} -> y
+#generate association rules that follow the format {a,b,c,...} -> y
 rules = {}		#(leftside, rightside) | frequency of leftside
-print "freqfreqs--------------------------\n",
-for k,v in freqfreqs.items():
-	print k, '-->', v
-
 for itemset in frequentsets:
 	if len(itemset) > 1:
 		dupe = copy.deepcopy(itemset)
 		for i in range(len(itemset)):
 			y = dupe.pop()
-			entry = (tuple(dupe), y)
-			print "f xUy", tuple(itemset), "is", freqfreqs[frozenset(itemset)]
-			print "f x", tuple(dupe), "is", freqfreqs[frozenset(dupe)]
-			print "support is ", freqfreqs[frozenset(itemset)]/float(freqfreqs[frozenset(dupe)])
-			rules[entry] = freqfreqs[frozenset(itemset)]/float(freqfreqs[frozenset(dupe)])
+			entry = (frozenset(dupe), y)
+			#conf of rule is (support of x U y)/(support of x)
+			confidence = freqfreqs[frozenset(itemset)]/float(freqfreqs[frozenset(dupe)])
+			supportx = freqfreqs[frozenset(itemset)]/float(len(biglist))
+			rules[entry] = [confidence, supportx]
 			dupe.insert(0, y)
 
-#confidence of an association rule x -> y is (support of x U y)/(support of x)
-#calculate the frequency to calculate the support, y frequencies are already in sets1
 # print "rules--------------------------\n"
 # for k,v in rules.items():
 # 	print k, '-->', v
@@ -117,6 +111,11 @@ for ru in rules:
 		listrules.append((ru, support))
 
 
+print "==Frequent itemsets (min_sup="+str(min_sup*100)+"%)"
+for k,v in freqfreqs.items():
+	support = v/float(len(biglist))
+	print str(list(k))+', '+str(int(support*100))+"%"
 
-for rule in listrules:
-	print rule[0][0], "->", rule[0][1], "-", rule[1]*100, "%"
+print "==High-confidence association rules (min_conf="+str(min_conf*100)+"%"
+for rule in sorted(listrules, key=itemgetter(1)):
+	print list(rule[0][0]), "=>", rule[0][1], "Conf:", str(int(rule[1][0]*100))+"%,", "Support:", str(int(rule[1][1]*100))+"%"
